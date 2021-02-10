37dc9fb4a3bd4114ee015cf093b47a3b3160c02f
mouna07 <mounamc261@gmail.com>
2021-02-10 16:14:40 +0530

    ADDED: code for Mobile Number Validation

--- src/com/BridgeLabz/UserRegistration.java
+++ src/com/BridgeLabz/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.BridgeLabz;
 
+
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -15,7 +16,10 @@ public class UserRegistration {
         System.out.println("Enter Last Name: ");
         userRegistration.nameValidation(scanner.next());
         System.out.println("Enter Email id: ");
-        userRegistration.emaileValidation(scanner.next());
+        userRegistration.emailValidation(scanner.next());
+        System.out.println("Enter Phone Number: ");
+        userRegistration.mobileNumberValidation(scanner.nextLine());
+        scanner.next();
 
     }
 
@@ -27,11 +31,20 @@ public class UserRegistration {
         }
     }
 
-    public void emaileValidation(String name) {
-        if (Pattern.matches("^[A-Za-z0-9_][A-Za-z0-9_.]*[@]{1}[a-z]+[.][a-z]{2,6}$", name)) {
+    public void emailValidation(String name) {
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\." + "[a-zA-Z0-9_+&*-]+)*@" + "(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
+        if (Pattern.matches(regex, name)) {
             System.out.println("Valid Email Id");
         } else {
             System.out.println("Invalid Email Id");
+        }
+    }
+
+    public void mobileNumberValidation(String num) {
+        if (Pattern.matches("^[1-9]{1,3}[ ]{1}[1-9]{1}[0-9]{9}$", num)) {
+            System.out.println("Valid Phone Number.");
+        } else {
+            System.out.println("Invalid Phone Number.");
         }
     }
 }

70a805a16968822f7b790a33f5c5b8a9d7c33578
mouna07 <mounamc261@gmail.com>
2021-02-10 16:01:22 +0530

    ADDED: code for validate both First and Last Name

--- src/com/BridgeLabz/UserRegistration.java
+++ src/com/BridgeLabz/UserRegistration.java
@@ -6,16 +6,22 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
+        UserRegistration userRegistration = new UserRegistration();
         String firstName;
 
         System.out.println("Welcome to User Registration!");
         System.out.println("Enter First Name: ");
-        firstName = scanner.next();
-        if (Pattern.matches("[A-Z][a-z]{2}[a-z]*", firstName)) {
-            System.out.println("Valid First Name");
-        } else {
-            System.out.println("Invalid First Name");
-        }
+        userRegistration.nameValidation(scanner.next());
+        System.out.println("Enter Last Name: ");
+        userRegistration.nameValidation(scanner.next());
 
+    }
+
+    public void nameValidation(String name) {
+        if (Pattern.matches("[A-Z][a-z]{2}[a-z]*", name)) {
+            System.out.println("Valid Name");
+        } else {
+            System.out.println("Invalid Name");
+        }
     }
 }
